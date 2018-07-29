ec9d9afff2be1c8b5543c7b7534a6bdc302406b6
Marceau Maubert <tenrys.iaido@live.fr>
2018-07-29 07:45:09 +0200

    ok there we go

--- src/commands/chatsounds/volume.js
+++ src/commands/chatsounds/volume.js
@@ -1,20 +1,15 @@
 module.exports = (category, bot) => {
     category.addCommand("volume", (msg, line, vol) => {
-        let vc = msg.guild.me.voiceChannel
+        let guild = msg.guild
 
-        if (vc && vc.connection && vc.connection.dispatcher) {
-            if (vol > 1 && vol <= 100) vol = vol / 100
-            let volume = Math.min(1, Math.max(0, vol))
+        if (vol > 1 && vol <= 100) vol = vol / 100
+        let volume = Math.min(1, Math.max(0, vol))
 
-            let playing = vc.connection.dispatcher
-            if (vol === undefined || vol === null || isNaN(vol) || isNaN(volume)) msg.result("Volume: " + playing.volume * 100 + "%.")
-            else {
-                vc.guild.volume = volume
-                playing.setVolume(vc.guild.volume)
-                msg.success("Changed volume to " + vc.guild.volume * 100 + "%.", category.printName)
-            }
-        } else {
-            msg.error("I am not in any channel.", category.printName)
+        if (vol === undefined || vol === null || isNaN(vol) || isNaN(volume)) msg.result("Volume: " + (guild.volume || 0.66) * 100 + "%.")
+        else {
+            guild.volume = volume
+            if (vc = guild.me.voiceChannel && vc.connection && vc.connection.dispatcher) vc.connection.dispatcher.setVolume(guild.volume)
+            msg.success("Changed volume to " + guild.volume * 100 + "%.", category.printName)
         }
     }, {
         aliases: [ "vol" ],
