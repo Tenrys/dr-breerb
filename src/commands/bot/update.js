1fe139a981c6003609f620f748894687d41f8a03
Marceau Maubert <tenrys.iaido@live.fr>
2018-07-30 16:18:50 +0200

    Truncate update

--- src/commands/bot/update.js
+++ src/commands/bot/update.js
@@ -19,7 +19,7 @@ module.exports = class UpdateCommand extends Command {
 
         let result = await runCommand("git pull")
 
-        await progressMsg.edit(`<@${msg.author.id}>, \`\`\`${result}\`\`\``, this.success("Done. See results for more information."))
+        await progressMsg.edit(`<@${msg.author.id}>, \`\`\`${this.bot.truncate(result)}\`\`\``, this.success("Done. See results for more information."))
 
         if (result.match("Updating") && !result.match("Aborting")) {
             progressMsg = await progressMsg.channel.messages.fetch(progressMsg.id) // Hack to make sure we have the right content, idk if this works
