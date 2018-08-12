a108da2171fb3eb542567861693347b02c95675a
Marceau Maubert <tenrys.iaido@live.fr>
2018-08-12 03:09:18 +0200

    Fix oversight for now

--- src/extensions/discord.js
+++ src/extensions/discord.js
@@ -6,4 +6,12 @@ Object.defineProperty(Discord.GuildMember.prototype, "voiceChannel", {
     }
 })
 
+Object.defineProperty(Discord.VoiceChannel.prototype, "members", {
+    get() {
+        return new Collection(this.guild.voiceStates
+            .filter(state => state.channelID === this.id && state.member)
+            .map(state => [state.id, state.member]))
+    }
+})
+
 module.exports = Discord
